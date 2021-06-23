1a112ce1842d869bb2722f2ac966df2e4258cdf4
Sarah Rainsberger <5098874+sarah11918@users.noreply.github.com>
2021-06-23 10:41:36 -0300

    Title change for new experiment

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,7 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Still coding on a Chromebook!</h1>
+    <h1>SCC take 2!</h1>
     <ul>
       <li><Link to="/blog/">Blog</Link></li>
       <li><Link to="/about/">About</Link></li>
