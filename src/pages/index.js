17058acb906d3e44bd21989d7cf9c677005651b0
Sarah Rainsberger <5098874+sarah11918@users.noreply.github.com>
2021-06-23 10:36:09 -0300

    trivial text change to trigger blog and see if it deploys to Netlify

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,7 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Coding on a Chromebook!</h1>
+    <h1>Still coding on a Chromebook!</h1>
     <ul>
       <li><Link to="/blog/">Blog</Link></li>
       <li><Link to="/about/">About</Link></li>
