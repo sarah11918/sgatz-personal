7dd06efe6a5d706ed65e044c75402b4e7f5a62a9
Sarah Rainsberger <5098874+sarah11918@users.noreply.github.com>
2021-06-26 11:16:53 -0300

    added Google Calendar embed to calendar page

--- src/pages/calendar.js
+++ src/pages/calendar.js
@@ -7,7 +7,17 @@ const Calendar = () => {
     <Layout>
       <SEO title="Summerside Choir Calendar" />
       <h1>Calendar</h1>
-      <p>Here is the choir's calendar.</p>
+      <p>Keep track of upcoming rehearsals, concerts and events!</p>
+      <div>
+      <iframe 
+      src="https://www.google.com/calendar/embed?showTitle=0&amp;showPrint=0&amp;showCalendars=0&amp;height=400&amp;wkst=1&amp;bgcolor=%23FFFFFF&amp;src=sumchoir%40gmail.com&amp;color=%232952A3&amp;ctz=America%2FHalifax"
+      width="98%" 
+      height="300" 
+      frameborder="0" 
+      scrolling="no" >
+
+      </iframe>
+      </div>
     </Layout>
   )
 }
